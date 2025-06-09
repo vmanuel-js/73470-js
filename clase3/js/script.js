ff377e77491958907c79262b0d09b610bb840802
Victor Jordan <vmjordansolis@gmail.com>
2025-06-09 18:59:38 -0500

    clase 3 inicio

---
+++ clase3/js/script.js
@@ -0,0 +1,4 @@
+// 1. Hacer un programa que permita ingresar 10 notas de alumno y sacar el promedio
+// 2. Modificar para que el usuario primero ingrese la cantidad de alumnos y luego sacar el promedio en base en eso.
+// 3. Modificar que siga haciendo lo del punto 2 pero a que a medida de ingresar la nota mencione si esta aprobado o no (>=7)
+// 4. Modificar el for del punto 3, pasarlo a while
